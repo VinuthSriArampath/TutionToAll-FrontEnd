4e82c859c88fd33669b27c8bb0a7b840c3e3171b
vinuth <vinuthsriarampath@outlook.com>
2024-11-03 21:36:31 +0530

    added the base router path for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginformComponent } from './pages/loginform/loginform.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginformComponent
+    }
+];
