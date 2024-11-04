062d69b53d3b6476bf654ec3922b558e7d24e3cf
vinuth <vinuthsriarampath@outlook.com>
2024-11-04 20:55:27 +0530

    Added the institute registration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginformComponent } from './pages/loginform/loginform.component';
+import { InstituteRegistrationComponent } from './pages/institute-registration/institute-registration.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LoginformComponent
+    },
+    {
+        path: 'institute/register',
+        component: InstituteRegistrationComponent  
     }
 ];
---
+++ src/app/pages/institute-registration/institute-registration.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-institute-registration',
+  standalone: true,
+  imports: [],
+  templateUrl: './institute-registration.component.html',
+  styleUrl: './institute-registration.component.css'
+})
+export class InstituteRegistrationComponent {
+  registrationNextStep(){
+    
+  }
+}
