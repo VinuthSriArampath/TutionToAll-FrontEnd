0b308ca7fa02b5c1805dbe0dfa2b07a780b10a87
vinuth <vinuthsriarampath@outlook.com>
2024-11-05 21:30:10 +0530

    Institute Registration Done

--- src/app/pages/institute-registration-otp/institute-registration-otp.component.ts
+++ src/app/pages/institute-registration-otp/institute-registration-otp.component.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { InstituteRegistrationComponent } from '../institute-registration/institute-registration.component';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-institute-registration-otp',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, HttpClientModule],
   templateUrl: './institute-registration-otp.component.html',
   styleUrl: './institute-registration-otp.component.css'
 })
 export class InstituteRegistrationOtpComponent {
+  public otp: any = "";
+  private institute: any;
 
+  constructor(private router: Router, private http: HttpClient) {
+    const institute = localStorage.getItem('instituteData');
+    if (institute) {
+      this.institute = JSON.parse(institute);
+    }
+  }
+
+  validateOtp() {
+    if (Number(this.otp) === Number(InstituteRegistrationComponent.getOTP())) {
+      try {
+        this.http.post("http://localhost:8080/institutes/register", this.institute).subscribe();
+        localStorage.removeItem('instituteData');
+        this.alertMessage("Institute Registered Successfully","success");
+        this.router.navigate(['']);
+      } catch(error) {
+        this.alertMessage("Something went wrong", "error");
+      }
+    } else {
+      this.alertMessage("Invalid OTP", "error");
+    }
+  }
+
+  public alertMessage(title: string, icon: any) {
+    swal.fire({
+      title: title,
+      icon: icon,
+      denyButtonText: "OK",
+    });
+  }
 }
--- src/app/pages/institute-registration/institute-registration.component.ts
+++ src/app/pages/institute-registration/institute-registration.component.ts
@@ -3,49 +3,49 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import swal from 'sweetalert2';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-institute-registration',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './institute-registration.component.html',
   styleUrl: './institute-registration.component.css'
 })
 export class InstituteRegistrationComponent {
+  public static OTP: any;
+  
   public institute:any={
-      instituteName:"",
-      contactNo:"",
-      emailAddress:"",
-      address:"",
-      loginPassword:"",
-      confirmPassword:"",
-  }
-  private router;
-  public OTP:String="000";
-  constructor(router:Router){
-    this.router=router
-  }
+    name:"",
+    email:"",
+    contact: '',
+    address: '',
+    password: '',
+  };
+  public confirmPassword:String="";
+  constructor(private router:Router, private http:HttpClient) {}
+
   validateDetails():boolean{
     const contactPattern=/^0[0-9]{9}$/;
     const emailPattern=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const passwordPattern=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d@$!%*?&]{8,}$/;
     
-    if(this.institute.instituteName==""){
+    if(this.institute.name==""){
       this.alertMessage("Institute Name is Required","error");
       return false;
-    }else if(!contactPattern.test(this.institute.contactNo)){
+    }else if(!contactPattern.test(this.institute.contact)){
       this.alertMessage("Contact Number is Invalid","error");
       return false;
-    }else if(!emailPattern.test(this.institute.emailAddress)){
+    }else if(!emailPattern.test(this.institute.email)){
       this.alertMessage("Email Address is Invalid","error");
       return false;
     }else if(this.institute.address==""){
       this.alertMessage("Address is Required","error");
-    }else if(!passwordPattern.test(this.institute.loginPassword)){
+    }else if(!passwordPattern.test(this.institute.password)){
       this.alertMessage("Password is Invalid","error"); 
       return false;
     }else{
-      if(this.institute.loginPassword!=this.institute.confirmPassword){
+      if(this.institute.password!=this.confirmPassword){
         this.alertMessage("Password and Confirm Password are not matched","error");
         return false;
       }else{
@@ -54,21 +54,37 @@ export class InstituteRegistrationComponent {
     }
     return false;
   }
+
   registrationNextStep(){
     if(this.validateDetails()){
-      console.log(this.institute);
-      this.alertMessage("Otp Has Been Sent To Your Email Address","info");
-      this.router.navigate(['/institute/registration/otp']);
+      try{
+        this.http.get(`http://localhost:8080/institutes/otp/${this.institute.email}`).subscribe(data => {
+          InstituteRegistrationComponent.OTP = data;
+        });
+        localStorage.setItem('instituteData', JSON.stringify(this.institute));
+        this.alertMessage("Otp Has Been Sent To Your Email Address","info");
+        this.router.navigate(['/institute/registration/otp']);
+      }catch(error){
+        this.alertMessage("Something Went Wrong","error");
+        return;
+      }
     }
   }
+
   public alertMessage(title:String,icon:any){
     swal.fire({
       title:title,
       icon:icon,
       denyButtonText:"OK",
     })
-  }
+  }  
+
+
   public registrationCancel(){
     this.router.navigate(['']);
+  }
+
+  public static getOTP(): any {
+    return InstituteRegistrationComponent.OTP;
   }
 }
