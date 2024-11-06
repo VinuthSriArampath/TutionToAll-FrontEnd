aac9e0bc2e516ee63d88cc2cc5c792a3a1348a30
vinuth <vinuthsriarampath@outlook.com>
2024-11-06 23:04:57 +0530

    Institute Register Teacher Component Ui

--- src/app/pages/teacher/institute-register-teacher/institute-register-teacher.component.ts
+++ src/app/pages/teacher/institute-register-teacher/institute-register-teacher.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../common/footer/footer.component";
+import { InstituteNavbarComponent } from "../../institute/common/institute-navbar/institute-navbar.component";
 
 @Component({
   selector: 'app-institute-register-teacher',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, InstituteNavbarComponent],
   templateUrl: './institute-register-teacher.component.html',
   styleUrl: './institute-register-teacher.component.css'
 })
