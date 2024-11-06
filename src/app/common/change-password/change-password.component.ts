a0f50744f841673648b45de5d4beb2480928253b
vinuth <vinuthsriarampath@outlook.com>
2024-11-06 08:58:15 +0530

    added the change password component

--- src/app/common/change-password/change-password.component.ts
+++ src/app/common/change-password/change-password.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Hearder1Component } from '../headers/hearder-1/hearder-1.component';
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [],
+  imports: [Hearder1Component],
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.css'
 })
