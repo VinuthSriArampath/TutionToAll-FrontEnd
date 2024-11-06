48e29e44563198990069fbdf7bdfea16dd5017d0
vinuth <vinuthsriarampath@outlook.com>
2024-11-06 17:44:07 +0530

    teacher navbar component

--- src/app/pages/teacher/common/teacher-navbar/teacher-navbar.component.ts
+++ src/app/pages/teacher/common/teacher-navbar/teacher-navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-teacher-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './teacher-navbar.component.html',
   styleUrl: './teacher-navbar.component.css'
 })
 export class TeacherNavbarComponent {
-
+  constructor(private router:Router){}
+  logout(){
+    sessionStorage.removeItem("LoggedUser");
+    this.router.navigate(['/']);
+  }
 }
--- src/app/pages/teacher/teacher-dashboard/teacher-dashboard.component.ts
+++ src/app/pages/teacher/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TeacherNavbarComponent } from "../common/teacher-navbar/teacher-navbar.component";
 
 @Component({
   selector: 'app-teacher-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TeacherNavbarComponent],
   templateUrl: './teacher-dashboard.component.html',
   styleUrl: './teacher-dashboard.component.css'
 })
