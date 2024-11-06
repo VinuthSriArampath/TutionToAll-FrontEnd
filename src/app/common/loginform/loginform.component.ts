aa6da976ae4ebbc2766f2bffec0bb94119f0a2ba
vinuth <vinuthsriarampath@outlook.com>
2024-11-06 14:11:16 +0530

    Get All Institutes,Students and Teachers On Landing Page

--- src/app/common/loginform/loginform.component.ts
+++ src/app/common/loginform/loginform.component.ts
@@ -4,6 +4,7 @@ import swal from 'sweetalert2';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Hearder1Component } from "../headers/hearder-1/hearder-1.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-loginform',
@@ -13,11 +14,15 @@ import { Hearder1Component } from "../headers/hearder-1/hearder-1.component";
   styleUrl: './loginform.component.css'
 })
 export class LoginformComponent {
-  private router: Router;
   public username:String = "";
   public password:String = "";
-  constructor(router: Router) {
-    this.router = router;
+  institutes: any[] = [];
+  students: any[] = [];
+  teachers: any[] = [];
+  constructor(private http: HttpClient,private router: Router) {
+    this.http.get('http://localhost:8080/institutes/all').subscribe(res => this.institutes = res as any[]);
+    this.http.get('http://localhost:8080/students/all').subscribe(res => this.students = res as any[]);
+    this.http.get('http://localhost:8080/teachers/all').subscribe(res => this.teachers = res as any[]);
   }
   login(){
     console.log();
