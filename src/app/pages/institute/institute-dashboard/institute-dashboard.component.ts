ee4ee4ee62de25e29ac97f49446e5863db3016d3
vinuth <vinuthsriarampath@outlook.com>
2024-11-06 19:07:57 +0530

    Added institute dashboard main content format

--- src/app/pages/institute/institute-dashboard/institute-dashboard.component.ts
+++ src/app/pages/institute/institute-dashboard/institute-dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { InstituteNavbarComponent } from '../common/institute-navbar/institute-navbar.component';
+import { FooterComponent } from "../../../common/footer/footer.component";
+import { AppComponent } from "../../../app.component";
 
 @Component({
   selector: 'app-institute-dashboard',
   standalone: true,
-  imports: [InstituteNavbarComponent],
+  imports: [InstituteNavbarComponent, FooterComponent, AppComponent],
   templateUrl: './institute-dashboard.component.html',
   styleUrl: './institute-dashboard.component.css'
 })
