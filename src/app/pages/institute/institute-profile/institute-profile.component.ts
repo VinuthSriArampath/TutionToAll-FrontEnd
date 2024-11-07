d4b378d2c3d8042dff66c18c7cf28158db705586
vinuth <vinuthsriarampath@outlook.com>
2024-11-07 16:08:25 +0530

    institute Profile Ui

--- src/app/pages/institute/institute-profile/institute-profile.component.ts
+++ src/app/pages/institute/institute-profile/institute-profile.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { InstituteNavbarComponent } from "../common/institute-navbar/institute-navbar.component";
+import { FooterComponent } from "../../../common/footer/footer.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-institute-profile',
   standalone: true,
-  imports: [],
+  imports: [InstituteNavbarComponent, FooterComponent,RouterLink],
   templateUrl: './institute-profile.component.html',
   styleUrl: './institute-profile.component.css'
 })
 export class InstituteProfileComponent {
-
+  editSwitch(){
+    console.log("Edit Switch");
+  }
 }
