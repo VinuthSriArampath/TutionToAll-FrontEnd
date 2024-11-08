1ccf8e3c2b73790d1498694ad0bc2792b20070bb
vinuth <vinuthsriarampath@outlook.com>
2024-11-08 07:45:00 +0530

    added teacher dashboard functionalities

--- src/app/pages/teacher/teacher-dashboard/teacher-dashboard.component.ts
+++ src/app/pages/teacher/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { TeacherNavbarComponent } from "../common/teacher-navbar/teacher-navbar.component";
+import { FooterComponent } from "../../../common/footer/footer.component";
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-teacher-dashboard',
   standalone: true,
-  imports: [TeacherNavbarComponent],
+  imports: [TeacherNavbarComponent, FooterComponent,FormsModule,CommonModule],
   templateUrl: './teacher-dashboard.component.html',
   styleUrl: './teacher-dashboard.component.css'
 })
 export class TeacherDashboardComponent {
+public instituteList:any[]=[];
+public instituteCourseList:any[]=[];
+constructor(private http:HttpClient){
+  const teacherId = JSON.parse(sessionStorage.getItem('LoggedUser') || '').id;
 
+  this.http.get(`http://localhost:8080/teachers/search/${teacherId}`).subscribe((teacher:any)=>{
+    for(let institute of teacher.registeredInstitutes){
+      this.http.get(`http://localhost:8080/institutes/search/${institute.instituteId}`).subscribe((institute:any)=>{
+        this.instituteList.push(institute);
+        let tempCourseArray:any[]=[];
+        for(let course of institute.courseList){
+          if(teacher.registeredCourses.find((c:any)=>c.id===course.id)){   
+            tempCourseArray.push(course);
+          }
+        }
+        this.instituteCourseList.push(tempCourseArray);
+      })
+    }   
+  })
+}
 }
