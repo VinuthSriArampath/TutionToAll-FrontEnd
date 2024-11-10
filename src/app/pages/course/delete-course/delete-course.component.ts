3c58bf2b09d8ee2394c89f67c55977f482e0a832
vinuth <vinuthsriarampath@outlook.com>
2024-11-10 22:22:38 +0530

    delete course component ui

--- src/app/pages/course/delete-course/delete-course.component.ts
+++ src/app/pages/course/delete-course/delete-course.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { InstituteNavbarComponent } from '../../institute/common/institute-navbar/institute-navbar.component';
+import { FooterComponent } from '../../../common/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-delete-course',
   standalone: true,
-  imports: [],
+  imports: [InstituteNavbarComponent,FooterComponent,FormsModule,CommonModule],
   templateUrl: './delete-course.component.html',
   styleUrl: './delete-course.component.css'
 })
