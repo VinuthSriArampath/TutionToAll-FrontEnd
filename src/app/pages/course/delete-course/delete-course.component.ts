fa6c6bf2b544051e8631667c24b0b58447567b41
vinuth <vinuthsriarampath@outlook.com>
2024-11-12 13:13:24 +0530

    removed model classes and Added delete Course functionalities

--- src/app/pages/course/delete-course/delete-course.component.ts
+++ src/app/pages/course/delete-course/delete-course.component.ts
@@ -3,6 +3,8 @@ import { InstituteNavbarComponent } from '../../institute/common/institute-navba
 import { FooterComponent } from '../../../common/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-delete-course',
@@ -17,9 +19,73 @@ import { CommonModule } from '@angular/common';
   styleUrl: './delete-course.component.css',
 })
 export class DeleteCourseComponent {
-  public searchedCourseId: String = '';
-  
-  public searchCourse(){
-
+  public searchedCourseId: String="";
+  public course : any ={
+    id:'',
+    name:'',
+    type:'',
+    teacherId:'',
+    teacherName:''
   }
+
+  public isCourseSearched: boolean = false;
+  private instituteId: string;
+
+  constructor(private http: HttpClient){
+    this.instituteId=JSON.parse(sessionStorage.getItem('LoggedUser') || '').id;  
+  }
+  public searchCourse(){
+    if (this.searchedCourseId == '') {
+      this.alert('Please enter a course id', 'error');
+    }else{
+      this.http.get(`http://localhost:8080/courses/search/${this.searchedCourseId}/institute/${this.instituteId}`).subscribe((res:any)=>{
+        if(res){
+          this.course=res;
+          this.isCourseSearched=true;
+        }else{
+          this.alert('Course not found on this '+this.instituteId, 'error');
+        }
+        
+      })
+    }
+  }
+
+  public deleteCourse(){
+    if (this.course.id == '') {
+      this.alert('Please search a course first', 'error');
+    }else{
+      Swal
+        .fire({
+          title: 'Do you want to save the changes?',
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: 'Yes',
+          denyButtonText: 'No',
+          customClass: {
+            actions: 'my-actions',
+            cancelButton: 'order-1 right-gap',
+            confirmButton: 'order-2',
+            denyButton: 'order-3',
+          },
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            this.http.delete(`http://localhost:8080/courses/delete/${this.course.id}/of/${this.instituteId}`).subscribe((res:any)=>{
+              this.alert('Course deleted successfully', 'success');
+            })
+          } else if (result.isDenied) {
+            Swal.fire('Course is not deleted', '', 'info');
+          }
+        });
+    }
+  }
+  private alert(title: string, type: any){
+    Swal.fire({
+      title: title,
+      icon: type,
+      confirmButtonText: 'OK',
+    });
+  }
+
+
 }
