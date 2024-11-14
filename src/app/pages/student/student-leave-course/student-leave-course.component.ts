40c6db175219cf19de44e6b72057d22911f14fb9
vinuth <vinuthsriarampath@outlook.com>
2024-11-14 19:44:03 +0530

    bug fix at course search in leave course component

--- src/app/pages/student/student-leave-course/student-leave-course.component.ts
+++ src/app/pages/student/student-leave-course/student-leave-course.component.ts
@@ -54,7 +54,7 @@ export class StudentLeaveCourseComponent {
     this.http.get(`http://localhost:8080/students/search/${JSON.parse(sessionStorage.getItem('LoggedUser') || '').id}`).subscribe((student:any)=>{
       for(let institute of student.registeredInstitutes){
         for(let course of institute.courses){
-          this.http.get(`http://localhost:8080/courses/getCourseById/${course.courseId}`).subscribe((resCourse:any)=>{
+          this.http.get(`http://localhost:8080/courses/getByCourseId/${course.courseId}`).subscribe((resCourse:any)=>{
             this.courseList.push(new Course(resCourse.id,institute.instituteId,institute.instituteName,resCourse.name,resCourse.type,resCourse.teacherId,resCourse.teacherName,course.date));
           })
         }
@@ -71,6 +71,7 @@ export class StudentLeaveCourseComponent {
       for(let course of this.courseList){
         if(course.courseId==this.searchedCourseId){
           this.searchedCourse=course;
+          this.isCourseSearched=true;
           found=true;
           break;
         }
