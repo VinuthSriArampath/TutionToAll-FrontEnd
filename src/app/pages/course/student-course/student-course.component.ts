0ec5b89a8035e630d7d67c507d963cf0a508be0d
vinuth <vinuthsriarampath@outlook.com>
2024-11-15 01:48:37 +0530

    student course component ui

--- src/app/pages/course/student-course/student-course.component.ts
+++ src/app/pages/course/student-course/student-course.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { StudentNavbarComponent } from "../../student/common/student-navbar/student-navbar.component";
+import { FooterComponent } from "../../../common/footer/footer.component";
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-student-course',
   standalone: true,
-  imports: [],
+  imports: [StudentNavbarComponent, FooterComponent,FormsModule,CommonModule],
   templateUrl: './student-course.component.html',
   styleUrl: './student-course.component.css'
 })
 export class StudentCourseComponent{
   public courseId:String='';
-  constructor(private activatedRoute:ActivatedRoute){
+  public course:any={
+    id:'',
+    name:'',
+    type:'',
+    teacherId:'',
+    teacherName:''
+  };
+  public assignmentList:any[]=[];
+  public noteList:any[]=[];
+  constructor(private activatedRoute:ActivatedRoute,private http:HttpClient){
     this.courseId=this.activatedRoute.snapshot.paramMap.get('id') || '';
+    this.http.get(`http://localhost:8080/courses/getByCourseId/${this.courseId}`).subscribe((res:any)=>{
+      console.log(res);
+      this.course=res;
+    })
+
   }
 
 }
