19aa1e8d7f31e17f39d43cfd468acaf87bf31fda
vinuth <vinuthsriarampath@outlook.com>
2024-11-18 10:08:27 +0530

    user authentication

--- src/app/common/loginform/loginform.component.ts
+++ src/app/common/loginform/loginform.component.ts
@@ -1,128 +1,68 @@
-import { Component, } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import swal from 'sweetalert2';
-import { Router, RouterLink, } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Hearder1Component } from "../headers/hearder-1/hearder-1.component";
+import { Hearder1Component } from '../headers/hearder-1/hearder-1.component';
 import { HttpClient } from '@angular/common/http';
 import { emit } from 'process';
+import { error } from 'console';
 
 @Component({
   selector: 'app-loginform',
   standalone: true,
   imports: [FormsModule, CommonModule, RouterLink, Hearder1Component],
   templateUrl: './loginform.component.html',
-  styleUrl: './loginform.component.css'
+  styleUrl: './loginform.component.css',
 })
 export class LoginformComponent {
-  public username:String = "";
-  public password:String = "";
-  institutes: any[] = [];
-  students: any[] = [];
-  teachers: any[] = [];
-  private LoggedUser:any={
-    id:"",
-    email:""
+  public LoginUser:any = {
+    userName:'',
+    password:''
   }
-  constructor(private http: HttpClient,private router: Router) {
-    this.http.get('http://localhost:8080/institutes/all').subscribe(res => this.institutes = res as any[]);
-    this.http.get('http://localhost:8080/students/all').subscribe(res => this.students = res as any[]);
-    this.http.get('http://localhost:8080/teachers/all').subscribe(res => this.teachers = res as any[]);
-  }
-  login(){
-    if(this.validateLogin()){
-
-      if (this.username.charAt(0) === "I") {  // Institute Login
-
-
-        const institute = this.institutes.find((ins:any)=>ins.id === this.username);
-        if(institute){
-          if(institute.password === this.password){
-          swal.fire({
-            title: "Login Successful!",
-            text: `Welcome ${institute.name}`,
-            icon: "success",
-            confirmButtonText: "Continue"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              this.LoggedUser.id = institute.id;
-              this.LoggedUser.email = institute.email;
-              sessionStorage.setItem("LoggedUser",JSON.stringify(this.LoggedUser));
-              this.router.navigate(['/institute/dashboard']);
-            }
-          });
-          }else{
-            this.alertMessage("Invalid Password","Please enter the correct password","error");
-          }
-        }else{
-          this.alertMessage("Invalid Username","Please enter the correct username","error");
-        }
-
-
-      }else if (this.username.charAt(0) === "S") {  // Student Login
-
-
-        const student = this.students.find((st:any)=>st.id === this.username);
-        if(student){
-          if(student.password === this.password){
-            swal.fire({
-            title: "Login Successful!",
-              text: `Welcome ${student.firstName} ${student.lastName}`,
-            icon: "success",
-            confirmButtonText: "Continue"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              this.LoggedUser.id = student.id;
-                this.LoggedUser.email = student.email;
-              sessionStorage.setItem("LoggedUser",JSON.stringify(this.LoggedUser));
-              this.router.navigate(['/student/dashboard']);
-            }
-          });
-          }else{
-            this.alertMessage("Invalid Password","Please enter the correct password","error");
-          }
-        }else{
-          this.alertMessage("Invalid Username","Please enter the correct username","error");
-        }
-
-
-      }else if (this.username.charAt(0) === "T") {  // Teacher Login
-
-
-        const teacher = this.teachers.find((t:any)=>t.id === this.username);
-        if (teacher) {
-          if(teacher.password === this.password){
-          swal.fire({
-            title: "Login Successful!",
-            text: `Welcome ${teacher.firstName} ${teacher.lastName}`,
-            icon: "success",
-            confirmButtonText: "Continue"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              this.LoggedUser.id = teacher.id;
-              this.LoggedUser.email = teacher.email;
-              sessionStorage.setItem("LoggedUser",JSON.stringify(this.LoggedUser));
-              this.router.navigate(['/teacher/dashboard']);
-            }
-          });
-        }else{
-          this.alertMessage("Invalid Password","Please enter the correct password","error");
-        }
-        }else{
-          this.alertMessage("Invalid Username","Please enter the correct username","error");
-        }
-        
-
-
+  private LoggedUser: any = {
+    id: '',
+  };
+  constructor(private http: HttpClient, private router: Router) {}
+  login() {
+    if (this.validateLogin()) {
+      if (this.LoginUser.userName.charAt(0) === 'I') {
+        this.http.post(`http://localhost:8080/auth/institute/login`, this.LoginUser).subscribe((res:any)=>{
+          this.LoggedUser.id=this.LoginUser.userName;
+          sessionStorage.setItem("LoggedUser",JSON.stringify(this.LoggedUser));
+          this.router.navigate(['/institute/dashboard']);
+        });
+      } else if (this.LoginUser.userName.charAt(0) === 'S') {
+        this.http.post(`http://localhost:8080/auth/student/login`, this.LoginUser).subscribe((res:any)=>{
+          this.LoggedUser.id=this.LoginUser.userName;
+          sessionStorage.setItem("LoggedUser",JSON.stringify(this.LoggedUser));
+          this.router.navigate(['/student/dashboard']);
+        });
+      }else if(this.LoginUser.userName.charAt(0) === 'T'){
+        this.http.post(`http://localhost:8080/auth/teacher/login`, this.LoginUser).subscribe((res:any)=>{
+          this.LoggedUser.id=this.LoginUser.userName;
+          sessionStorage.setItem("LoggedUser",JSON.stringify(this.LoggedUser));
+          this.router.navigate(['/teacher/dashboard']);
+        });
+      } else {
+        this.alertMessage('Invalid UserName', '', 'error');
       }
     }
   }
-  validateLogin():boolean{
-    if(!this.username){
-      this.alertMessage("Username is Required","Please enter your username","error");
+  validateLogin(): boolean {
+    if (!this.LoginUser.userName) {
+      this.alertMessage(
+        'Username is Required',
+        'Please enter your username',
+        'error'
+      );
       return false;
-    }else if(!this.password){
-      this.alertMessage("Password is Required","Please enter your password","error");
+    } else if (!this.LoginUser.password) {
+      this.alertMessage(
+        'Password is Required',
+        'Please enter your password',
+        'error'
+      );
       return false;
     }
     return true;
